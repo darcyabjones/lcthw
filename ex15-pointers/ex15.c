71c90b86cc38f2c454eccd52d65c9e032a0a4762
Darcy Jones <darcy.ab.jones@gmail.com>
2018-04-14 18:09:56 +0800

    ex15

---
+++ ex15-pointers/ex15.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // create two arrays we care about
+
+    int ages[] = { 23, 43, 12, 89, 2 };
+    char *names[] = {
+        "Alan", "Frank",
+        "Mary", "John",
+        "Lisa",
+    };
+
+    // Safely get the size of ages
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
+
+    // First way using indexing
+    for (i = 0; i < count; i++) {
+        printf("%s has %d years alive.\n", names[i], ages[i]);
+    }
+
+    printf("---\n");
+
+    //setup the pointers to the start of the arrays
+    int *cur_age = ages;
+    char **cur_name = names;
+
+    // Second way using pointers
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
+    }
+
+    // third way, pointers are just arrays
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years old.\n", cur_name[i], cur_age[i]);
+    }
+
+    // Stupid complicated way
+    for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++) {
+        printf("%s lived %d years so far. loc: %p\n", *cur_name, *cur_age, cur_name);
+    }
+
+    return 0;
+}
